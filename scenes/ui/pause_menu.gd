07393357231de4fa5baac7f89bed076105766ec1
neonvoidx <me@neonvoid.dev>
2026-01-18 10:45:52 -0500

    Pause menu complete

--- scenes/ui/pause_menu.gd
+++ scenes/ui/pause_menu.gd
@@ -1,8 +1,14 @@
-extends Node
+extends Control
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var panel_container : PanelContainer = $PanelContainer
+@onready var color_rect : ColorRect = $ColorRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if panel_container:
+		panel_container.hide()
+	if color_rect:
+		color_rect.hide()
 	if animation_player:
 		animation_player.play("RESET")
 
@@ -14,8 +20,17 @@ func resume():
 	GameState.unpause()
 	if animation_player:
 		animation_player.play_backwards("blur")
+	await animation_player.animation_finished
+	if panel_container:
+		panel_container.hide()
+	if color_rect:
+		color_rect.hide()
 
 func pause():
+	if color_rect:
+		color_rect.show()
+	if panel_container:
+		panel_container.show()
 	GameState.pause()
 	if animation_player:
 		animation_player.play("blur")
--- scripts/autoload/game_state.gd
+++ scripts/autoload/game_state.gd
@@ -20,8 +20,7 @@ func complete_level() -> void:
 func pause() -> void:
 	is_paused = true
 	get_tree().paused = true
-	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
-
+	
 func unpause() -> void:
 	is_paused = false
 	get_tree().paused = false
