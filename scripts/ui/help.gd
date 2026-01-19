5955ea2fefb530ff94125b0bbe90a277afedd504
neonvoidx <me@neonvoid.dev>
2026-01-19 14:19:10 -0500

    assets

--- scripts/ui/help.gd
+++ scripts/ui/help.gd
@@ -7,6 +7,6 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if GameState.is_puzzle_complete:
 		self.hide()
